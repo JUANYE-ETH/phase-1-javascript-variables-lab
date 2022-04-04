90fa986f470827e912d5b8d9947a0851e8a48b2c
Juan Velasco <juanayevela@yahoo.com>
2022-04-04 11:40:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+// Declaring Company Name using const
+
+let mostProfitableNeighborhood = 'Chelsea';
+// Declaring the most profitbale neighborhood using let
+
+let companyCeo = 'Susan Smith';
+// Declaring the compant ceo using let
